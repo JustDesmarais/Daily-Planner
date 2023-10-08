529a57225c6ce965352e8b48b01797e025c34e86
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-10-08 15:07:04 -0500

    properly logging parentNode id

--- js/script.js
+++ js/script.js
@@ -9,13 +9,18 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   // 
-  /**$('button').click(function (e) {
-   *   $('.description')})
-   */
+  var plans = []
+
   $('.btn').click(function () {
     console.log($(this).siblings('textarea').val());
+    console.log($(this).parent().attr('id')); 
   });
-  //
+  /**
+   * variables to store once clicked:
+   * text value
+   * id ('hour-x) of the time-block chosen
+   * calendar day of when the save was logged
+   */
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
